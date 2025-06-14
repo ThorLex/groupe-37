0db1fb97c7bfe6b4a32664946f142c3eabd0df62
Ivan <ivannteumi@gmail.com>
2025-06-14 20:14:45 +0100

    start enrollment page

--- frontend/app/suivi/page.tsx
+++ frontend/app/suivi/page.tsx
@@ -1,9 +1,81 @@
-import React from 'react'
+"use client"
 
-const page = () => {
+import SearchBar from '@/components/macro-element/SearchBar'
+import Image from 'next/image'
+import { useState } from 'react'
+
+const mockData = {
+  requestNumber: 'LT/A/22/03/25/00001',
+  lastName: 'ATANGANA',
+  firstName: 'Jean Pierre',
+  submissionDate: '22/03/2025',
+  biometricsDate: '04/04/2025',
+  biometricsStatus: 'en attente',
+  pickupDate: 'N/A',
+  status: 'En attente',
+};
+
+const StatusPage = () => {
+  const [data, setData] = useState<typeof mockData | null>(null)
+  const [error, setError] = useState('')
+
+  const handleSearch = (query: string) => {
+    if (query.trim() === mockData.requestNumber) {
+      setData(mockData);
+      setError('');
+    } else {
+      setData(null);
+      setError("Aucune demande trouvée pour ce numéro.");
+    }
+  };
+
   return (
-    <section className='md:h-[80vh]'>Suivre une demande</section>
+    <div>
+      <section>
+        <SearchBar onSearch={handleSearch}/>
+      </section>
+
+      <main className='flex justify-center'>
+          <div className='bg-gray-100 rounded-xl shadow-lg p-6 w-full max-w-8/12 mb-8'>
+            <div className='flex sm:flex-row flex-col sm:space-x-6'>
+              <div className='shrink-0 mb-4 h-40 w-38 relative sm:mb-0'>
+                <Image
+                  src="/4.png"
+                  alt='Photo du demandeur'
+                  fill
+                  className='rounded-lg'
+                />
+              </div>
+              {data ? (
+                <div className="flex-grow text-gray-900 ml-3">
+                  <p><span className="font-semibold">N° demande:</span> <span className='font-bold'>{data.requestNumber}</span></p>
+                  <p className="mt-5"><span className="font-semibold">Noms:</span> <span className='font-bold'>{data.lastName}</span> </p>
+                  <p className="mt-5"><span className="font-semibold">Prénoms:</span> <span className='font-bold'>{data.firstName}</span></p>
+                  <p className="mt-5"><span className="font-semibold">Date de soumission:</span> <span className='font-bold'>{data.submissionDate}</span></p>
+                  <p className="mt-5 text-gray-400"><span className="font-semibold">Date de biométrie:</span> <span className='font-bold'>{data.biometricsDate}</span> ({data.biometricsStatus})</p>
+                  <p className="mt-5"><span className="font-semibold">Date de retrait de la CNI:</span> <span className='font-bold'>{data.pickupDate}</span></p>
+                </div>
+              ) : error ? (
+                <> 
+                  <p className="text-red-500 font-semibold">{error}</p>
+                </>
+              ) : (
+                <> 
+                  <p className="mt-2 text-center text-gray-600 dark:text-gray-400">
+                    Entrez votre numéro de demande pour afficher le statut de votre CNI.
+                  </p>
+                </>
+              )}
+            </div>
+            {data && (
+              <div className="mt-6 text-right text-gray-900">
+                <span className="font-semibold">Statut:</span> ⏳ {data.status}
+              </div>
+            )}  
+          </div>
+      </main>
+    </div>
   )
 }
 
-export default page
+export default StatusPage
---
+++ frontend/components/preenrol/ConfirmationStep.tsx
@@ -0,0 +1,41 @@
+'use client'
+
+import { CheckCircle } from 'lucide-react'
+import { motion } from 'framer-motion'
+import { useRouter } from 'next/navigation'
+
+export default function ConfirmationStep() {
+  const router = useRouter()
+
+  const restart = () => {
+    router.refresh()
+  }
+
+  return (
+    <motion.div
+      initial={{ opacity: 0, y: 30 }}
+      animate={{ opacity: 1, y: 0 }}
+      className="text-center space-y-6 p-6"
+    >
+      <CheckCircle size={60} className="text-green-500 mx-auto" />
+      <h2 className="text-2xl font-semibold text-green-500">Pré-enrôlement réussi !</h2>
+      <p className="text-white">Nous avons bien reçu vos informations. Vous recevrez un email de confirmation d&lsquo;ici peu.</p>
+
+      <div className="flex justify-center gap-4 pt-4">
+        <button
+          onClick={() => router.push('/')}
+          className="px-4 py-2 bg-blue-600 text-white rounded hover:bg-blue-700 transition cursor-pointer"
+        >
+          Accueil
+        </button>
+
+        <button
+          onClick={restart}
+          className="px-4 py-2 border border-blue-600 text-blue-600 rounded hover:bg-blue-50 transition cursor-pointer"
+        >
+          Nouveau pré-enrôlement
+        </button>
+      </div>
+    </motion.div>
+  )
+}
